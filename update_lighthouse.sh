c8896f0a8e66d787fe1f5f193fd481a7e3baa160
superphiz <51464737+superphiz@users.noreply.github.com>
2021-01-22 16:17:54 -0600

    Update update_lighthouse.sh

--- update_lighthouse.sh
+++ update_lighthouse.sh
@@ -4,6 +4,7 @@ BLUE='\033[0;34m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
+printf "Run this as a normal user without sudo because it uses home directories";
 
 printf "Go here ${BLUE}https://github.com/sigp/lighthouse/releases${NC}, copy the link for ${YELLOW}x86_64-unknown-linux-gnu-portable${NC}.tar.gz
 
@@ -27,7 +28,7 @@ $HOME/lighthouse --version
 
 printf "${YELLOW}CHECK FOR THE CORRECT VERSION NUMBER ABOVE BEFORE ACCEPTING THIS UPDATE! FAILING TO VERIFY THIS WILL BREAK THINGS!${NC}\n\n";
 
-echo "Do you want to stop the lighthouse beacon chain and validator to update it? "
+printf "Do you want to stop the lighthouse beacon chain and validator to update it? ";
 select yn in "Yes" "No"; do
     case $yn in
         Yes )
@@ -40,6 +41,8 @@ select yn in "Yes" "No"; do
 
 	sudo mv $HOME/lighthouse /usr/local/bin/
 
+	sudo systemctl daemon-reload
+	
 	sudo systemctl start lighthousebeacon
 
 	sudo systemctl start lighthousevalidator
